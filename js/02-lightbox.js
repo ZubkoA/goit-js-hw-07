d40a330fb81d3b181de2f9102200b5c7ec5dcbaa
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-03-04 13:28:55 +0200

    finish0403

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,16 +7,19 @@ galleryWrapEl.insertAdjacentHTML("beforeend", cardsImg);
 
 function creatImgCards(galleryItems) {
   return galleryItems
-    .map(({ preview, original }) => {
+    .map(({ preview, original, description }) => {
       return `
       <div class="gallery__item">
     <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="Image description" />
+  <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
         </div>
         `;
     })
     .join("");
 }
-const galleryIMG = new SimpleLightbox(".gallery a");
+const galleryIMG = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryIMG);
